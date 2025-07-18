f67a62ac28ca93a4fcfc05172b90122c0287d942
Fadhel642 <fadhelg845@gmail.com>
2025-07-18 05:20:08 +0200

    Update admission_filter.py

--- admission_filter.py
+++ admission_filter.py
@@ -1,1 +1,5 @@
+import imaplib                            # permet de se connecter à une boîte mail via le protocole IMAP.
+import email                              # sert à lire et analyser les e-mails récupérés.
+from email.header import decode_header
+import re
 
